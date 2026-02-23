7931a1a50d3eef152e19a4573cdcd739b51a3461
zuka910-arch <zuka910@gmail.com>
2026-02-23 17:41:07 +0000

    grade system

---
+++ conditionals/compare.py
@@ -0,0 +1,29 @@
+x = int( input("whats x? "))
+y = int(input ("whats y? "))
+
+
+
+
+
+
+
+
+
+
+
+#if x < y:
+#    print("x is less than y")
+#elif x > y:
+#    print("x is greater than y")
+#else:
+#    print("x is equal to y")
+
+
+
+#if x < y:
+ #   print("x is less than y")
+#elif x > y:
+#    print("x is greater than y")
+#elif x == y:
+#    print("x is equal y") 
+    
