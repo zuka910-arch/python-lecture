4140d327298eee18bad2fc18750752b5bf511b58
zuka910-arch <zuka910@gmail.com>
2026-03-11 22:50:47 +0000

    add  hogwarts tip test.py

---
+++ loops/test.py
@@ -0,0 +1,10 @@
+workers = {
+    "giorgi": "900euro",
+    "zuka": " 1300euro",
+    "tamuna": " 1500euro",
+    "shako": " 1600euro"
+}
+
+
+for worker in workers:
+    print(worker, workers[worker], sep=", ხელფასი - ")
