437f3254d69f489d392e3070507d3f2e690b97c6
zuka910-arch <zuka910@gmail.com>
2026-03-14 12:16:34 +0000

    update

--- loops/hogwarts.py
+++ loops/hogwarts.py
@@ -21,10 +21,45 @@ students = {
 
 
 
-for student in students:
+#for student in students:
 
      # ვერსია 1
     #print(student, ",", " ",  students[student], sep="")
 
         # ვერსია 2 იგივეა როგოც ვერსია 1 მაგრამ უფრო მარტივი   
-    print(student, students[student], sep=", ")
+    #print(student, students[student], sep=", ")
+
+
+
+
+#for k, v in students.items():
+#    print(k,v, sep=", ")
+
+
+
+"""
+
+| #      |  name        |  house      | patronus  |
+---------------------------------------------------
+| 0      | Hermione     | Gryffindor  | Otter     |
+| 1      | Harry        | Gryffindor  | Stag      |
+
+"""
+
+
+
+students = [
+    {"name" : "Hermione", "house" : "Gryffindor", "patronus" : "Otter"},
+    {"name" : "Harry", "house" : "Gryffindor", "patronus" : "Stag"},
+    {"name" : "Ron", "house" : "Gryffindor" , "patronus" : "Jack Russell Terrier"},
+    {"name" : "Draco","house" : "Slytherin", "patronus" : "None"}
+]
+
+#for student in students:
+#    print(student["name"], student["house"], student["patronus"], sep=", ")
+
+for student in students:
+    if student["house"] == "Gryffindor":
+        print(student["name"], student["house"], student["patronus"], sep=", ")
+
+        
--- loops/test.py
+++ loops/test.py
@@ -1,10 +1,8 @@
-workers = {
-    "giorgi": "900euro",
-    "zuka": " 1300euro",
-    "tamuna": " 1500euro",
-    "shako": " 1600euro"
+d = {
+    "students": [ "harry", "hermione", "ron" ],
 }
+print(d["students"])
 
+d["students"].append("draco")
 
-for worker in workers:
-    print(worker, workers[worker], sep=", ხელფასი - ")
+print("after", d["students"])
