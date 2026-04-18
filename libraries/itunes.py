90201926a90fce0f4b2b74d1c78be3ec1c871f8b
zuka910-arch <zuka910@gmail.com>
2026-04-18 10:51:13 +0000

    add sayings_imp_say1.py and sayings.py

--- libraries/itunes.py
+++ libraries/itunes.py
@@ -2,7 +2,7 @@ import json
 import sys
 import requests
 
-if len(sys.argv) != 2:
+if len(sys.argv) == 2:
     sys.exit()
 
 response = requests.get("https://itunes.apple.com/search?entity=song&limit=200&term=" + sys.argv[1])
