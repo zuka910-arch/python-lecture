543c4d5f195b11fee78004e70e739df0881841ba
zuka910-arch <zuka910@gmail.com>
2026-05-18 17:56:25 +0000

    ex8.py

--- oop/ex8.py
+++ oop/ex8.py
@@ -14,15 +14,22 @@ class User:
 
     def great_user(self):
         print(f"hello,{self.first_name}")
+
+class Privileges:
+    def __init__(self):
+        self.privileges = ["can add post","can delete post","can ban user","can edit post"]
+    def show_privileges(self):
+        print("admin privileges")
+
+        for privilege in self.privileges:
+            print(f"{privilege}")
+
+
 class Admin(User):
     def __init__(self, first_name, last_name, user_age, user_nationality):
         super().__init__(first_name, last_name, user_age, user_nationality)
-        self.privileges= ["can add post", "can delete post", "can ban user", "can edit post"]
+        self.privileges= Privileges()
 
-    def show_privileges(self):
-        print("admin privileges")
-        for privilege in self.privileges:
-            print(f"{privilege}")
 admin1 = Admin("zuka", "bakuradze", 25, "georgia")
 admin1.describe_user()
-admin1.show_privileges()
+admin1.privileges.show_privileges() 
