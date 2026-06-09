542e6418bd18e92115ee84c088852d7b67c8ac46
zuka910-arch <zuka910@gmail.com>
2026-06-09 22:06:48 +0000

    Aggiungi menu interattivo per gestione camere con validazione input

--- python-test/oop/hotel_system.py
+++ python-test/oop/hotel_system.py
@@ -9,8 +9,24 @@ class HotelRoom:
     def check_out(self):
         self.is_clean = False
         print(f"Il cliente è uscito! La camera {self.room_number} richiede lo sbarazzo!")
-rooms_database = {}
 
+#HOTEL menu
+def get_num(prompt):
+    while True:
+        try:
+            num = int(input(prompt))
+            if num  <= 0 or num > 125 :
+                print("Per favore, inserisci un numero di camera valido (1-125)!") 
+                continue
+
+            else:
+             return num
+        except ValueError:
+
+            print("Inserisci solo numeri validi!")
+            continue
+
+rooms_database = {}
 while True:
     print("--- 🛎️ Pannello di Controllo Camere --- \n" \
         "1. Check-in (Aggiungi camera) \n" \
@@ -18,18 +34,15 @@ while True:
         "3. Esci dal programma \n")
     choose = input("Scegli un'opzione (1/2/3): ")
     if choose == "1":
-        num = input("room numer: ")
-        rooms_database[num] = HotelRoom(num)
-        rooms_database[num].check_in()
-
+            num = get_num("Numero di camera: ")
+            rooms_database[num] = HotelRoom(num)
+            rooms_database[num].check_in()
     elif choose == "2":
-        num = input("Quale camera ha fatto il check-out?")
-        if num in rooms_database:
-            rooms_database[num].check_out()
-        else:
-            print("Camera non trovata!")
-
-            
+            num = get_num("Quale camera ha fatto il check-out?   ")
+            if num in rooms_database:
+                rooms_database[num].check_out()
+            else:
+                print("Camera non trovata!")
     elif choose == "3":
         print("arrivederci")
         break
